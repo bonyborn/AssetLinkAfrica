d20cb095b268c083bebf1a0554d5f673146bba23
Boniface  Muvea <ezekielbon2@gmail.com>
2026-04-13 14:47:36 +0300

    Update urls.py

--- AssetLinkAfrica/AssetLinkAfrica/urls.py
+++ AssetLinkAfrica/AssetLinkAfrica/urls.py
@@ -15,9 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+admin.site.site_header="AssetLinkAfrica Administration"
+admin.site.site_title="Welcome to AssetLinkAfrica"
+admin.site.index_title="Welcome to AssetLinkAfrica"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path('', include('home.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

decba44c31ae2fed2cf367565529616ae24641c8
Boniface  Muvea <ezekielbon2@gmail.com>
2026-04-13 16:09:53 +0300

    Update views.py

--- AssetLinkAfrica/Home/views.py
+++ AssetLinkAfrica/Home/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+  
+def admin(request):
+    return render(request, 'admin.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def transactions(request):
+    return render(request, 'transactions.html')
+  

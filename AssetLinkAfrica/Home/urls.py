ebd596bcc860e28ea470e51299519b787fd8a1af
Boniface  Muvea <ezekielbon2@gmail.com>
2026-04-13 16:03:07 +0300

    Change home view to index in URL patterns
    Updated the home URL patterns to use 'index' view.

--- AssetLinkAfrica/Home/urls.py
+++ AssetLinkAfrica/Home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('login/', views.login, name='login'),

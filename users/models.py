8d327bb7c5571f76063ea56b9ff547665536ae2d
Boniface  Muvea <ezekielbon2@gmail.com>
2026-05-28 11:40:14 +0300

    Add custom User model with additional fields

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    phone_number = models.CharField(
+        max_length=20,
+        blank=True
+    )
+
+    profile_image = models.ImageField(
+        upload_to='profiles/',
+        blank=True,
+        null=True
+    )
+
+    is_verified = models.BooleanField(
+        default=False
+    )
+
+    def __str__(self):
+        return self.username

b819a8729e055289e1fe2b7a4c2d59b09288213f
Boniface  Muvea <ezekielbon2@gmail.com>
2026-05-28 11:35:36 +0300

    Update settings.py

--- AssetLinkAfrica/settings.py
+++ AssetLinkAfrica/settings.py
@@ -120,6 +120,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
+
 # files files
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/

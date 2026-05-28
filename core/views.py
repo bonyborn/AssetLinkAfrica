9e89a6572d134aeba6b580a04a19f76de1829e3b
Boniface  Muvea <ezekielbon2@gmail.com>
2026-05-28 13:32:54 +0300

    Update home view to include latest listings
    Fetch the latest listings and pass them to the template.

--- core/views.py
+++ core/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-
+from listings.models import Listing
 
 def home(request):
 
+    latest_listings = Listing.objects.all().order_by('-created_at')[:6]
+
     return render(
         request,
-        'core/index.html'
+        'core/index.html',
+        {'latest_listings': latest_listings}
     )

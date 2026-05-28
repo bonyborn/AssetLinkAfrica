795e3e02ebc38f150af40adccbb8d523d5d96ed4
Boniface  Muvea <ezekielbon2@gmail.com>
2026-05-28 11:59:25 +0300

    Add URL pattern for creating bookings

---
+++ bookings/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+
+    path(
+        'create/<int:id>/',
+        views.create_booking,
+        name='create_booking'
+    ),
+
+]

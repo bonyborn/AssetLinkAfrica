eb92d789fc0a1aea714aa971a15b9b0d2fd35314
Boniface  Muvea <ezekielbon2@gmail.com>
2026-05-28 11:50:23 +0300

    Add create_booking view with login requirement

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+
+from listings.models import Listing
+
+from .models import Booking
+
+
+@login_required
+def create_booking(request, id):
+
+    listing = get_object_or_404(
+        Listing,
+        id=id
+    )
+
+    booking = Booking.objects.create(
+        user=request.user,
+        listing=listing
+    )
+
+    return redirect(
+        'checkout',
+        booking.id
+    )

85e0eca480e9a140764c9389cd422a4599ca5a2a
Boniface  Muvea <ezekielbon2@gmail.com>
2026-05-28 11:49:20 +0300

    Add filtering and create listing functionality

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,89 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+
+from .models import Listing
+from .forms import ListingForm
+
+
+def listings(request):
+
+    listings = Listing.objects.all()
+
+    query = request.GET.get('q')
+
+    category = request.GET.get('category')
+
+    location = request.GET.get('location')
+
+    if query:
+        listings = listings.filter(
+            title__icontains=query
+        )
+
+    if category:
+        listings = listings.filter(
+            category=category
+        )
+
+    if location:
+        listings = listings.filter(
+            location__icontains=location
+        )
+
+    context = {
+        'listings': listings
+    }
+
+    return render(
+        request,
+        'listings/listings.html',
+        context
+    )
+
+
+@login_required
+def create_listing(request):
+
+    if request.method == 'POST':
+
+        form = ListingForm(
+            request.POST,
+            request.FILES
+        )
+
+        if form.is_valid():
+
+            listing = form.save(commit=False)
+
+            listing.owner = request.user
+
+            listing.save()
+
+            return redirect('listings')
+
+    else:
+
+        form = ListingForm()
+
+    return render(
+        request,
+        'listings/create.html',
+        {'form': form}
+    )
+
+
+def details(request, id):
+
+    listing = get_object_or_404(
+        Listing,
+        id=id
+    )
+
+    return render(
+        request,
+        'listings/details.html',
+        {'listing': listing}
+  )

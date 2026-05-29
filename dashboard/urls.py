c3237d76f1a16d3bdc66f1d14e0fa67cbff9411d
Boniface  Muvea <ezekielbon2@gmail.com>
2026-05-29 14:56:06 +0300

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,2 +1,12 @@
 from django.urls import path
-from . import viviviewsththhhhhhpathaththhhhhhhhhhhhththhhhh
+from . import views
+
+urlpatterns = [
+
+    path(
+        '',
+        views.dashboard,
+        name='dashboard'
+    ),
+
+]
